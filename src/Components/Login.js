9bee6f7be0b6bd5d874e46f6b006158bbe8010e1
ariful007 <arifulislamwd007@gmail.com>
2023-03-26 15:53:24 +0600

    loginupdate

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,19 +1,47 @@
 
 import React from 'react';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import app from '../Firabase/FirabaseConfig';
 
 const Login = () => {
+
+    
+    const auth = getAuth(app);
+    const handelLogin = (e) =>{
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value
+        const password= form.password.value
+        signInWithEmailAndPassword(auth,email,password)
+        .then((userCredential) => {
+            toast.success('Login Successfully')
+            const user = userCredential.user;
+            console.log(user)
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorMessage)
+          });
+        console.log(email,password)
+      }
+
+
     return (
         <div className='container login_section'>
+            <ToastContainer />
             <div className='wrapper'>
              <h1 className='header'>Login Here</h1>
-              <form id='register' method='post'>
+              <form onSubmit={handelLogin}>
                 <div className='login-form'>
                     <div className='label'>Email address</div>
-                    <input type="email" placeholder="Enter email" />
+                    <input name='email' type="email" placeholder="Enter email" />
  
                     <div className='label'>Password</div>
-                    <input type="password" placeholder="Password" />
-                    <button>Login</button>
+                    <input name='password' type="password" placeholder="Password" />
+                    <button type='submit'>Login</button>
                 </div>
                </form>
             </div>
--- src/Components/Registration.js
+++ src/Components/Registration.js
@@ -1,18 +1,53 @@
-import React from 'react';
+import React  from 'react';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import app from '../Firabase/FirabaseConfig';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate } from 'react-router-dom';
+
 
 const Registration = () => {
+  const navigate = useNavigate()
+  const auth = getAuth(app);
+
+
+  const handelSignUp = (e) =>{
+    e.preventDefault()
+    const from = e.target;
+    const email = from.email.value
+    const password =from.password.value
+    createUserWithEmailAndPassword(auth, email, password)
+
+    .then((userCredential) => {
+      toast.success('Registration Successfully')
+      navigate('/login')
+      const user = userCredential.user;
+      console.log(user)
+      from.reset()
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage)
+    });
+     console.log(email,password)
+  }
+
+
+
   return (
     <div className='container login_section'>
+       <ToastContainer />
         <div className='wrapper'>
              <h1 className='header'>Registration Here</h1>
-              <form id='register' method='post'>
+              <form onSubmit={handelSignUp} >
                 <div className='login-form'>
                     <div className='label'>Email address</div>
-                    <input type="email" placeholder="Enter email" />
+                    <input name='email' type="email" placeholder="Enter email" />
  
                     <div className='label'>Password</div>
-                    <input type="password" placeholder="Password" />
-                    <button>Sign Up</button>
+                    <input name='password' type="password" placeholder="Password" />
+                    <button type='submit' >Sign Up</button>
                 </div>
                </form>
         </div>
